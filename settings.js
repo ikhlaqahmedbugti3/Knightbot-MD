daec2f148392511d174d64aa852fadca08ba5f80
ikhlaqahmedbugti3 <ikhlaqahmedbugti3@gmail.com>
2025-07-31 11:05:02 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Knight DP',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "Knigt DP",
   botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '+923334104708', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
