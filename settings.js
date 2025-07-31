b174f9efc642ef52b6368b33dc0137b41646c260
ikhlaqahmedbugti3 <ikhlaqahmedbugti3@gmail.com>
2025-07-31 11:06:53 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,11 +2,11 @@ const settings = {
   packname: 'Knight DP',
   author: '‎',
   botName: "Knigt DP",
-  botOwner: 'Professor', // Your name
+  botOwner: 'Smilleee', // Your name
   ownerNumber: '+923334104708', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "Whatsapp Robot By Ikhlaq Ahmed.",
   version: "2.0.8",
 };
 
